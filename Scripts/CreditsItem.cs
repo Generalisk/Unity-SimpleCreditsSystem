5a9a7f40676eed2fc07055cda20b0993d746ed66
Generalisk <80116907+Generalisk@users.noreply.github.com>
2025-10-29 15:48:17 +0000

    Update CreditsItem.cs

--- Scripts/CreditsItem.cs
+++ Scripts/CreditsItem.cs
@@ -10,6 +10,12 @@ namespace Generalisk.Credits
 
         public Sprite image = null;
 
+        // TODO: Resolve depth limit warnings
+        //
+        // They're mostly harmless unless
+        // you go that deep (which is unlikely
+        // to happen without doing so intentionally)
+        // but the warnings can get annoying
         public CreditsItem[] subItems = { };
     }
 }
